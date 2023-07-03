171fdc38e8e52c15011949f037098b15f303421c
Mejtus1 <GitMaster@proton.me>
2023-07-03 09:53:23 +0200

    C++ file + first lesson(consoole)

---
+++ C++.Tutorial.cpp
@@ -0,0 +1,13 @@
+//C++ Programming 
+//comments are written like in javascript using double /
+
+cout << "Hello World"; //cout << is used to output the code to the console
+cout << "My name is Codio";
+
+cout << "Hello"; //outputs code on one line and together 
+cout << "world!"; //Helloworld!
+cout << endl; //endl = command which adds the newline character.
+cout << "My name is Codio." << endl; //returns string on new line 
+
+cout << "Hi!" << endl; //The "Hi!" will print the text Hi! and the endl; will add a newline character. 
+
