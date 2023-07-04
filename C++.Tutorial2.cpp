2fb54d4a6a2650441c69a59943dd2c1e4a0333d6
Mejtus1 <GitMaster1769@proton.me>
2023-07-04 12:24:14 +0200

    string concatenation, variables incrementation

--- C++.Tutorial2.cpp
+++ C++.Tutorial2.cpp
@@ -46,3 +46,49 @@ printf("%f \n", b); //1.000000 = float specifier results in 0.000000 decimal for
 
 //-----------------------------------------------------------------------------------------------------------//
 //-----------------------------------------------------------------------------------------------------------//
+//Incrementing variables 
+//Incrementing means increasing value of the variable by 1
+int a = 0;
+a = a + 1; //typical variable incrementation by itself +1
+cout << a << endl; //2
+
+//a++ 
+//a++ does the same thing as a = a + 1
+int a = 0;
+int b = 0;
+a = a + 1;         //a++ (a = a + 1)
+b++;               //b++ (b = b + 1)
+cout << a << endl;
+cout << b << endl;
+
+int c = 2;
+c+=5; // c = c + 5 (7)
+c-=1; // c = c - 1 (1)
+
+
+//String concatenation 
+//Combining two strings together using (+ operator)
+string a = "This is an ";
+string b = "example string";
+string c = a + b; //concatenates a + b variables which store strings 
+cout << c << endl;
+
+string a = "This is an ";
+string b = "example string";
+string c = a+=b; //does nothing outputs same thing like code above
+cout << c << endl;
+
+string a = "This is an ";
+string b = "example string";
+string c = a + b + 3; //error - 3 is an integer and we have data type string 
+cout << c << endl;
+
+string a = "This is an ";
+string b = "example string";
+string c = a + b + "3"; //outputs 3 as an string 
+cout << c << endl;
+
+
+
+//-----------------------------------------------------------------------------------------------------------//
+//-----------------------------------------------------------------------------------------------------------//
