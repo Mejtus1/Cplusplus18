b5d4fc3ce7da72609516d4304ea85d25f8f32b58
Mejtus1 <GitMaster1769@proton.me>
2023-07-04 12:20:26 +0200

    addition

--- C++.Tutorial2.cpp
+++ C++.Tutorial2.cpp
@@ -1,1 +1,21 @@
 //Arithmetic Operators
+//Addition 
+cout << 7 + 3 << endl; //10
+
+int a = 7; //two variables
+int b = 3;
+cout << a + b << endl; //10
+
+  double a = 7.1; //double 
+  int b = 3;
+  cout << a + b << endl; //10.1
+
+  double a = 7.1;
+  int b = -3; //negative number
+  cout << a + b << endl; //4.1
+
+  double a = 7.1;
+  int b = +3; //explicitly positive number 
+  cout << a + b << endl; //10.1 (normal addition)
+
+
