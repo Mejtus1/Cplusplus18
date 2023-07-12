4608f826a5fa646784efa3aedf722340a096aec0
Mejtus1 <GitMaster1769@proton.me>
2023-07-12 05:35:57 +0200

    turtle graphics using C++

--- C++.Tutorial4.cpp
+++ C++.Tutorial4.cpp
@@ -36,3 +36,58 @@ for (int i = 0; i <= 5; i++) { //we can also use <= operator and the output will
 for (int i = 0; i <= 5; i--) { // we can change i++ to i--, but because no stop condition is 
   cout << "Loop #" << i << endl; //specified, the loop would run forever
 }
+
+
+
+//TURTLE GRAPHICS
+//it is an C++ extension that can draw lines
+//purpose of this is to create simple and graphical interfaces using code
+//header info which needs to be included in order to run CPP.turtletina
+#include <iostream>
+#include "CTurtle.hpp"
+#include "CImg.h"
+using namespace cturtle;
+using namespace std;
+
+//typical commands are 
+tina.forward(n)
+tina.backward(n)
+tina.right(n)
+tina.left(n)
+
+
+//this prints a line 
+TurtleScreen screen(400, 300);
+Turtle tina(screen);
+tina.forward(100);
+screen.exitonclick();
+
+//this prints a square
+TurtleScreen screen(400, 300);
+Turtle tina(screen);
+tina.forward(100);
+tina.right(90);
+tina.forward(100);
+tina.right(90);
+tina.forward(100);
+tina.right(90);
+tina.forward(100);
+tina.right(90);
+
+//exercise with for loops 
+//these codes produce exact square
+for (int i = 0; i < 4; i++) {
+  tina.forward(100);
+  tina.right(90);
+}
+
+for (int i = 1; i < 5; i++) {
+  tina.forward(100);
+  tina.right(90);
+}
+
+for (int i = 20; i < 24; i++) {
+  tina.forward(100);
+  tina.right(90);
+}
+
