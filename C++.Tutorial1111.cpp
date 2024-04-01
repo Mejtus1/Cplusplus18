8bed161d6cff0f13823c246ab780e1abe5db6599
Mejtus1 <GitMaster1769@proton.me>
2024-04-01 22:48:08 +0200

    Getters and Setters

--- C++.Tutorial1111.cpp
+++ C++.Tutorial1111.cpp
@@ -450,3 +450,360 @@ class Dog {
 // The convention in C++ is to always make instance attributes private.
 // Some functions need to be public while others can be private. Only functions needed by the user should be public; the rest should be private.
 
+//---------------------------------------------------------------------------------------------------//
+// Getters 
+// - while all attributes are private, that does not mean that user will not need to access values of these attributes
+// - public function can be used to access private attribute
+// - this type of function has special name, getter (also called an accessor)
+// - whole purpose of getter is to return attribute
+// - these functions get their name because they always start with Get followed by attribute name
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+//add class definitions below this line
+    
+class Phone {
+  public:
+    Phone(string mo, int s, int me) {
+      model = mo;
+      storage = s;
+      megapixels = me;
+    }
+
+    string GetModel() {
+      return model;
+    }
+  
+  private:
+    string model;
+    int storage;
+    int megapixels;
+};
+
+//add class definitions above this line
+
+// - function GetModel is example of getter
+// - Getters are very simple, straightforward functions that do only one thing — return private attribute
+// - getters can be treated just as you would treat attribute (except for changing its value)
+// - to test getter, you can call in main to see if it returns what is expected
+
+int main() {
+  
+  //add code below this line
+
+  Phone my_phone("iPhone", 256, 12);
+  cout << my_phone.GetModel() << endl;
+
+  //add code above this line
+  
+  return 0;
+  
+}
+// Iphone 
+
+// Try this ! 
+// - create getters for storage and megapixels attributes
+// - then call them within main
+
+//class definitions
+public:
+  int GetStorage() {
+    return storage;
+  }
+
+  int GetMegapixels() {
+    return megapixels;
+  }
+
+//main function
+cout << my_phone.GetStorage() << endl;
+cout << my_phone.GetMegapixels() << endl;
+
+//----------------------
+// Benefits of getters 
+// - using getter is same thing as accessing public attribute
+// - why not make the attribute public? That would mean writing less code
+// - is that not good thing? A public attribute makes no distinction between accessing its value and changing its value
+// - if you can access it, you can change it (or vice versa)
+// - using getter with private attribute makes this distinction clear; you can access value, but you cannot change it
+
+  //add code below this line
+
+  Phone my_phone("iPhone", 256, 12);
+  cout << my_phone.GetModel() << endl;
+  my_phone.model = "Pixel 5";
+
+  //add code above this line
+
+// - code above generates error because instance cannot alter private attribute
+// - using getter allows limited access to an attribute, which is preferable to the full access public accessmodifier allows
+
+// Semi-exercise Getters 
+// - drag code blocks into box below, create best getter function for language attribute
+class Country {
+  public:
+    Country(string n, vector<string>& l) {
+      name = n;
+      languages = l;
+    }
+
+  private:
+    string name;
+    vector<string> languages;
+};
+
+// answer: 
+public:
+vector<string> GetLanguages() {
+return languages;
+}
+
+// - Getter functions are accessible to user, so they need to have public access modifier
+// vector<string> GetLanguages() is only function that has correct return type (string vector)
+// - getter function should do only one thing, return value of attribute
+// - printing should not be done by getter function
+
+//---------
+// Setters 
+// - setters are compliment to getters in that they allow you to set value of private attribute
+// - setter functions are also called mutators
+// - use Phone class from previous page
+
+//add class definitions below this line
+    
+class Phone {
+  public:
+    Phone(string mo, int s, int me) {
+      model = mo;
+      storage = s;
+      megapixels = me;
+    }
+
+    string GetModel() {
+      return model;
+    }
+
+    int GetStorage() {
+      return storage;
+    }
+
+    int GetMegapixels() {
+      return megapixels;
+    }
+  
+  private:
+    string model;
+    int storage;
+    int megapixels;
+};
+  
+//add class definitions above this line
+
+// - add SetModel method to Phone class
+// - as this is setter method, start name with Set followed by name of attribute
+// - setters do not return anything
+// - finally, setters have parameter — new value for attribute
+
+  public:
+    // Setter
+    void SetModel(string new_model) {
+      model = new_model;
+    }
+
+Now that you are implementing both getters and setters, you should now be able to access and modify private attributes.
+
+  //add code below this line
+
+  Phone my_phone("iPhone", 256, 12); 
+  cout << my_phone.GetModel() << endl;
+  my_phone.SetModel("XR");
+  cout << my_phone.GetModel() << endl;
+  
+  //add code above this line
+
+
+// Try this ! 
+// - create setters for storage and megapixels attributes, then call them within main
+//class definitions
+public:
+  void SetStorage(int new_storage) {
+    storage = new_storage;
+  }
+
+  void SetMegapixels(int new_megapixels) {
+    megapixels = new_megapixels;
+  }
+
+//main function
+my_phone.SetStorage(128);
+cout << my_phone.GetStorage() << endl;
+my_phone.SetMegapixels(6);
+cout << my_phone.GetMegapixels() << endl;
+
+//------------------------------
+// Comparing Getters and Setters
+// - 
+// Category	             Getters	Setters
+// Has public keyword	      X	       X
+// Has private keyword	    -	       -
+// Has return statement	    X	       -
+// Has void type	          -	       X
+// Performs only 1 task	    X	       X
+// Has parameter	          -	       X
+
+// Semi-Exercise
+// Given following code:
+class Country {
+  public:
+    Country(string n, vector<string>& l) {
+      name = n;
+      languages = l;
+    }
+
+  private:
+    string name;
+    vector<string> languages;
+};
+
+// answer: 
+public:
+void SetLanguages(vector<string>& new_languages) {
+languages = new_languages;
+}
+// Setter functions do not return a value, so their type should be void.
+// Setter functions update an attribute to a new value, so they require a parameter that represents this new value.
+// Setter functions need to be public so they can be called by users of the class.
+
+//----------------
+// Data Validation
+// - C++ already has type system which will flag errors when boolean value is passed to function that takes integer
+// - however, just because function takes integer does not mean that all integers are valid for that function
+// - data validation is process of asking if this data is appropriate for its intended use
+// - take look at the Person class
+
+//add class definitions below this line
+    
+class Person {
+  public :
+    Person(string n, int a) {
+      name = n;
+      age = a;
+    }
+
+    string GetName() {
+      return name;
+    }
+
+     void SetName(string new_name) {
+      name = new_name;
+    }
+
+     int GetAge() {
+      return age;
+    }
+
+     void SetAge(int new_age) {
+      age = new_age;
+    }
+  
+  private: 
+    string name;
+    int age;
+};
+  
+//add class definitions above this line
+
+// - SetAge function will assign any value to attribute age as long as value is integer
+// - there are some integers which make no sense when thought of as age attribute
+// - code sample below sets age of my_person to -100. -100 is valid integer, but it is not valid age
+// - this is why data validation is important
+// - C++’s compiler is not sufficient to catch all errors
+
+  //add code below this line
+
+  Person my_person("Calvin", 6);
+  cout << my_person.GetName() << " is " << my_person.GetAge() << " years old." << endl;
+  my_person.SetAge(-100);
+  cout << my_person.GetName() << " is " << my_person.GetAge() << " years old." << endl;
+
+  //add code above this line
+
+// - another benefit of using setters is that data validation can take place before new value is assignedto attribute
+// - modify SetAge so that it will only update age attribute if new value is greater than or equal to 0
+// - this way you can ensure that Person object always has valid age
+
+  void SetAge(int new_age) {
+    if (new_age >= 0) {
+      age = new_age;
+    }
+  }
+
+// Try these ! 
+// - change data validation for SetAge function so that values over 200 or less than 0 are not valid
+  void SetAge(int new_age) {
+    if (new_age >= 0 && new_age <= 200) {
+      age = new_age;
+    }
+  }
+
+// - add data validation to SetName function so that all strings with one or more characters are valid
+ void SetName(string new_name) {
+    if (new_name != "") {
+      name = new_name;
+    }
+  }
+
+// Semi-Exercise 
+// Data Validation
+// Select all of the true statements about data validation.
+// a) Data validation is important because the C++ compiler cannot find all errors
+// b) Data validation is done in setter method
+
+// Data validation is done in a setter method
+// Data validation is important because the C++ compiler cannot find all errors
+
+// - job of getter function is to return value of attribute
+// - these functions should never update value of attribute, so data validation should not be done in getter function
+// - C++ compiler will find certain kinds of errors like incorrect data types
+// - however, compiler does not always understand context of code
+// - data validation is process of making sure value is appropriate for its intended use
+
+//-----------
+// Exercise 1
+// Getter and Setter Terms
+// - fill in blanks below with correct terms
+
+// A getter returns the value of a private instance variable.
+// A setter changes the value of a private instance variable.
+
+//-----------
+// Exercise 2 
+// Getters and Setters Die
+// Given class DieRoll:
+
+class DieRoll {
+  public:
+    int GetRoll() {
+      return roll;
+    }
+
+  private:
+    int roll = 0;
+};
+
+// - drag and rearrange code blocks below to create setter that validates if rolled number is between 1 and 6 inclusive
+// - if it is, roll will be updated to number that is rolled
+// - you MUST include AT LEAST ONE access modifier in your code
+
+// answer: 
+public:
+  void SetRoll(int new_roll) {
+    if (new_roll > 0 && new_roll <= 6) {
+      roll = new_roll;
+    }
+  }
+// Setter functions should be public so users can access them
+// Setter functions should start with void and include parameter that will be passed to it
+// if parameter or argument is between 0 and 6 inclusive, roll attribute will be updated to what was rolled
