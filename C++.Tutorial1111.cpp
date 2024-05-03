0fd28e9a1d19070dcd7681cfd164e7f582980c25
Mejtus1 <GitMaster1769@proton.me>
2024-05-04 00:01:58 +0200

    Exercise 2

--- C++.Tutorial1111.cpp
+++ C++.Tutorial1111.cpp
@@ -1351,3 +1351,122 @@ class Fruit {
     string name;
     string color;
 };
+
+//---------------------------------------------------------------------------------------------------//
+// Exercise 2 
+
+// - create Watch class following conventions of encapsulation
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  Watch my_watch("Omega", "Speedmaster", 42, 50, "steel");
+  cout << my_watch.GetManufacturer() << endl;
+  cout << my_watch.GetModel() << endl;
+  cout << my_watch.GetDiameter() << endl;
+  cout << my_watch.GetWaterResistance() << endl;
+  cout << my_watch.GetMaterial() << endl;
+  my_watch.SetManufacturer("Rolex");
+  my_watch.SetModel("Explorer");
+  my_watch.SetDiameter(36);
+  my_watch.SetWaterResistance(60);
+  my_watch.SetMaterial("gold");
+  my_watch.Summary();
+  
+  return 0;
+  
+}
+
+// Requirements
+// - create Watch class with following attributes:
+// manufacturer - string that represents who makes watch
+// model - string that represents specific watch
+// diameter - integer that represents (in millimeters) diameter of watch
+// water_resistance - integer that represents (in meters) water resistance of watch
+// material - string that represents what watch is made of
+
+// Expected Output
+// Omega
+// Speedmaster
+// 42
+// 50
+// steel
+// Manufacturer: Rolex
+// Model: Explorer
+// Diameter: 36 mm
+// Water Resistance: 60 m
+// Material: gold
+
+// - connect constructor parameters to private attributes
+// - setters are void functions, but make sure correct return type is labeled for each getter
+
+// Solution:
+
+class Watch {
+  public:
+    Watch(string mf, string mo, int d, int w, string mt) {
+      manufacturer = mf;
+      model = mo;
+      diameter = d;
+      water_resistance = w;
+      material = mt;
+    }
+  
+    string GetManufacturer() {
+      return manufacturer;
+    }
+
+    void SetManufacturer(string new_manufacturer) {
+      manufacturer = new_manufacturer;
+    }
+
+    string GetModel() {
+      return model;
+    }
+
+    void SetModel(string new_model) {
+      model = new_model;
+    }
+
+    int GetDiameter() {
+      return diameter;
+    }
+
+    void SetDiameter(int new_diameter) {
+      diameter = new_diameter;
+    }
+
+    int GetWaterResistance() {
+      return water_resistance;
+    }
+
+    void SetWaterResistance(int new_water_resistance) {
+      water_resistance = new_water_resistance;
+    }
+
+    string GetMaterial() {
+      return material;
+    }
+
+    void SetMaterial(string new_material) {
+      material = new_material;
+    }
+
+    void Summary() {
+      cout << "Manufacturer: " << manufacturer << endl;
+      cout << "Model: " << model << endl;
+      cout << "Diameter: " << diameter << " mm" << endl;
+      cout << "Water Resistance: " << water_resistance << " m" << endl;
+      cout << "Material: " << material << endl;
+    }
+  
+  private:
+    string manufacturer;
+    string model;
+    int diameter;
+    int water_resistance;
+    string material;
+};
+
