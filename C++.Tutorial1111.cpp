1f5a569dd4b63bc429f185595bec89aabdd3c4f1
Mejtus1 <GitMaster1769@proton.me>
2024-05-05 13:16:08 +0200

    Exercise 3

--- C++.Tutorial1111.cpp
+++ C++.Tutorial1111.cpp
@@ -1470,3 +1470,139 @@ class Watch {
     string material;
 };
 
+
+//-----------
+// Exercise 3
+
+// - complete Song class which represents song on streaming service
+
+#include <iostream>
+using namespace std;
+
+class Song {
+
+  private:
+    string artist;
+    string title;
+    string album;
+    int play_count = 0;
+    const double pay_rate = 0.001;
+    double money_earned = 0;
+    
+    void UpdatePlayCount() {
+      play_count++;
+    }
+  
+    void UpdateMoneyEarned() {
+      money_earned = play_count * pay_rate;
+    }
+};
+
+int main() {
+
+  Song my_song("Led Zeppelin", "Ten Years Gone", "Physical Graffiti");
+  cout << my_song.GetArtist() << endl;
+  cout << my_song.GetTitle() << endl;
+  cout << my_song.GetAlbum() << endl;
+  cout << my_song.GetPlayCount() << endl;
+  cout << my_song.GetPayRate() << endl;
+  cout << my_song.GetMoneyEarned() << endl;
+  my_song.SetArtist("Michael Jackson");
+  my_song.SetTitle("Beat It");
+  my_song.SetAlbum("Thriller");
+  my_song.Play(1000000);
+  my_song.Stats();
+  
+  return 0;
+  
+}
+
+// Requirements
+// - complete Song class that includes all necessary getter and setter functions for each class attribute
+// - there should be total of 6 getter functions
+// - there should be total of 3 setter functions
+// play_count, pay_rate and money_earned do not have setter functions because they are initialized to 0 or 0.001 when Song object is instantiated
+// - include class function Play that takes in an int parameter and calls UpdatePlayCount and UpdateMoneyEarned that number of times
+// - include class function Stats that prints all of class attributes according to their declaration order
+// - include appropriate access modifier(s) in your class
+
+// Expected Output
+// Led Zeppelin
+// Ten Years Gone
+// Physical Graffiti
+// 0
+// 0.001
+// 0
+// Michael Jackson
+// Beat It
+// Thriller
+// 1000000
+// 0.001
+// 1000
+
+// Key Points:
+
+// - connect constructor parameters to appropriate private attributes; not all of private attributes have to linked
+// - label getters and setters appropriately; some setters are not needed
+// - Play and Stats are void functions where Play calls the appropriate functions to update play_count and money_earned while Stats prints out requested output
+
+// Solution:
+
+  public:
+    Song(string ar, string t, string al) {
+      artist = ar;
+      title = t;
+      album = al;
+    }
+  
+    string GetArtist() {
+      return artist;
+    }
+
+    void SetArtist(string new_artist) {
+      artist = new_artist;
+    }
+
+    string GetTitle() {
+      return title;
+    }
+
+    void SetTitle(string new_title) {
+      title = new_title;
+    }
+
+    string GetAlbum() {
+      return album;
+    }
+
+    void SetAlbum(string new_album) {
+      album = new_album;
+    }
+
+    int GetPlayCount() {
+      return play_count;
+    }
+
+    double GetMoneyEarned() {
+      return money_earned;
+    }
+
+    double GetPayRate() {
+      return pay_rate;
+    }
+
+    void Play(int count) {
+      for (int i = 0; i < count; i++) {
+        UpdatePlayCount();
+        UpdateMoneyEarned();
+      }
+    }
+  
+    void Stats() {
+      cout << artist << endl;
+      cout << title << endl;
+      cout << album << endl;
+      cout << play_count << endl;
+      cout << pay_rate << endl;
+      cout << money_earned << endl;
+    }
