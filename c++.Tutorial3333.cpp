1174fb7afe2497ae988826cb34e6b85b4c9afa0b
Mejtus1 <GitMaster1769@proton.me>
2024-05-12 14:58:27 +0200

    Exercise 5

--- c++.Tutorial3333.cpp
+++ c++.Tutorial3333.cpp
@@ -913,3 +913,108 @@ class Median {
 // - then arguments should be sorted (i.e. sort(nums.begin(), nums.end());)
 // - if there are three or five arguments, “middle” number should be returned
 // - if there are four arguments, average of two “middle” numbers should be returned; remember to cast return value as double
+
+//-----------
+// Exercise 5 
+
+// - create Substitute class which is derived class of base class Words
+// - substitute has just one attribute called input_string and function called SubstituteChar which takes in string and replaces every fifth character with string HELLO
+
+// example:
+// - if word is Morning, then function will return MornHELLOng
+// - if word is Teddy, then function will return TeddHELLO
+// - if word has fewer than 5 characters, then message Not enough characters will be returned
+
+// Given code: 
+#include <iostream>
+#include <vector>
+using namespace std;
+
+//DO NOT EDIT///////////////////////////////////////////
+class Words {                                         //
+  public:                                             //
+    Words(string s1) {                                //
+      original = s1;                                  //
+    }                                                 //
+                                                      //
+    string SubstituteChar() {                         //
+      string sub;                                     //
+      if (original.length() < 3) {                    //
+        return "Not enough characters";               //
+      }                                               //
+      else {                                          //
+        for (int i = 0; i < original.length(); i++) { //
+          if (i == 2) {                               //
+            sub += "HELLO";                           //
+          }                                           //
+          else {                                      //
+            sub += original.at(i);                    //
+          }                                           //
+        }                                             //
+      }                                               //
+      return sub;                                     //
+    }                                                 //
+                                                      //
+  private:                                            //
+    string original;                                  //
+};                                                    //
+////////////////////////////////////////////////////////
+
+//add class definitions below this line
+
+
+
+//add class definitions above this line
+
+
+int main() {
+  
+  //DO NOT EDIT code below this line
+  
+  Substitute s1("dog");
+  cout << s1.SubstituteChar() << endl;
+  Substitute s2("string");
+  cout << s2.SubstituteChar() << endl;
+  Words w("string");
+  cout << w.SubstituteChar() << endl;
+  
+  //DO NOT EDIT code above this line
+  
+  return 0;
+  
+}
+
+// Expected Result
+// Not enough characters
+// striHELLOg
+// stHELLOing
+
+// Solution:
+
+class Substitute : public Words {
+  public:
+    Substitute(string s2) : Words(s2) {
+      input_string = s2;
+    }
+  
+    string SubstituteChar() {
+      string sub;
+      if (input_string.length() < 5) {
+        return "Not enough characters";
+      }
+      else {
+        for (int i = 0; i < input_string.length(); i++) {
+          if (i == 4) {
+            sub += "HELLO";
+          }
+          else {
+            sub += input_string.at(i);
+          }
+        }
+      }
+      return sub;
+    }
+  
+  private:
+    string input_string;
+};
