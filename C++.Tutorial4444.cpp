200d296d108edee60da0584b6100981f388e99e0
Mejtus1 <GitMaster1769@proton.me>
2024-05-18 00:12:17 +0200

    Exercise 5

--- C++.Tutorial4444.cpp
+++ C++.Tutorial4444.cpp
@@ -1434,3 +1434,75 @@ class Person {
 // - variable name and function GetName are accessed without Person object; therefore, they should both be defined as static in Person class
 // - since all of class members are public, you can alternatively use struct
 
+//-----------
+// Exercise 5 
+
+// - create static function called CompareStudents that takes in two Student objects as parameters and checks to see if their attributes (name and student_id) are same
+// - if objects’ attributes are same, true is returned, otherwise, false is returned
+
+#include <iostream>
+using namespace std;
+
+//DO NOT EDIT/////////////////////
+class Student {                 //
+  public:                       //
+    Student(string n, int id) { //
+      name = n;                 //
+      student_id = id;          //
+    }                           //
+//////////////////////////////////
+  
+  //add definitions below this line
+  
+
+  
+  //add definitions above this line
+  
+//DO NOT EDIT/////////
+  private:          //
+    string name;    //
+    int student_id; //
+};                  //
+//////////////////////
+
+int main() {
+  
+  //DO NOT EDIT code below this line
+
+  Student andy("Andy Ace", 123456);
+  Student anderson("Anderson Ace", 123456);
+  Student ace("Andy Ace", 123456);
+  Student andrew("Andy Ace", 234567);
+  cout << boolalpha;
+  cout << Student::CompareStudents(andy, anderson) << endl;
+  cout << Student::CompareStudents(andy, ace) << endl;
+  cout << Student::CompareStudents(andy, andrew) << endl;
+
+  //DO NOT EDIT code above this line
+  
+  return 0;
+  
+}
+
+// Expected Output
+// false
+// true
+// false
+
+// Solution:
+
+    static bool CompareStudents(Student s1, Student s2) {
+      if ((s1.name == s2.name) &&
+          (s1.student_id == s2.student_id)) {
+        return true;
+      }
+      else {
+        return false;
+      }
+    }
+
+// - compareStudents should be static since it is called using scope resolution operator :: in main
+// - make sure CompareStudents takes in two Student objects
+// - set up conditionals to compare attributes name and student_id of Student objects
+// - if attributes are  same, return true, otherwise, return false
+// - compareStudents can be either bool or string function as both will work with the code in main
