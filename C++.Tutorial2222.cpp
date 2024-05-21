54690c8a0c90932a08ed16c1f7465ba1371760f6
Mejtus1 <GitMaster1769@proton.me>
2024-05-21 18:14:41 +0200

    Base and Derived Classes 3

--- C++.Tutorial2222.cpp
+++ C++.Tutorial2222.cpp
@@ -294,6 +294,224 @@ class Derived : public Base {
 // Because Derived is a derived class of Base, it will have access to all public and protected class members of Base. 
 // Therefore, it can access both x and y.
 
+//-----------------------------------------------------------------------------------------------------------//
+// file 2222, week 2 1/5 (Base and Derived Classes 3) 
+
+// How Inheritance Works
+
+#include <iostream>
+using namespace std;
+
+class Base {
+  public:
+    void Public(string s) {
+      s = "public";
+      cout << s << endl;
+    }
+  
+  protected:
+    void Protected(string s) {
+      s = "protected";
+      cout << s << endl;
+    }
+    
+  private:
+    void Private(string s) {
+      s = "private";
+      cout << s << endl;
+    }
+};
+
+//add class definitions below this line
+
+
+
+//add class definitions above this line
+
+int main() {
+  
+  //add code below this line
+  
+  string s_main;
+  Derived dc;
+  dc.ReturnProtected(s_main);
+  dc.Public(s_main);
+
+  //add code above this line
+  
+  return 0;
+  
+}
+
+// Now that you’re more comfortable with access modifiers, let’s take a look at how inheritance types affect derived classes. 
+// Make sure your Derived and main code look like what’s below. 
+// Note that Derived now inherits from Base through protected inheritance instead of public.
+//add class definitions below this line
+
+class Derived : protected Base {
+  public:
+    void ReturnPublic(string s) {
+      Public(s_derived);
+    }
+  
+    void ReturnProtected(string s) {
+      Protected(s_derived);
+    }
+  
+  private:
+    string s_derived;
+};
+
+//add class definitions above this line
+  //add code below this line
+  
+  string s_main;
+  Derived dc;
+  dc.ReturnProtected(s_main);
+  dc.Public(s_main);
+
+  //add code above this line
+
+// You’ll notice that an error is produced saying that ‘Base’ is not an accessible base of ‘Derived’. This occurs because when a derived class inherits from a base class through protected inheritance, all public and protected members of the base class become protected in the derived class. 
+// This means that the Public function within Derived was inherited as a protected function and is therefore no longer accessible within main.
+// On the other hand, ReturnProtected is still a public function within Derived, which allows main to access it even though it calls the protected function Protected from Base. 
+// Comment out the command dc.Public(s_main); and run the code again to see the result.
+// Though not explicitly shown, Derived can be represented like this:
+
+class Derived : protected Base {
+  public:
+    void ReturnPublic(string s) {
+      Public(s_derived);
+    }
+  
+    void ReturnProtected(string s) {
+      Protected(s_derived);
+    }
+  
+  /*protected:
+    void Public(string s) { (inherited as protected from Base)
+      s = "public";
+      cout << s << endl;
+    }
+  
+    void Protected(string s) { (inherited as protected from Base)
+      s = "protected";
+      cout << s << endl;
+    }*/
+  
+  private:
+    string s_derived;
+};
+
+// If you ever get confused with inheritance types, you can always imaginatively rewrite all of the functions that were inherited and label them appropriately to help you visualize the derived class better. Here are some more examples:
+
+class Base {
+  public:
+    int x;
+  
+  protected:
+    int y;
+  
+  private:
+    int z;
+};
+
+class Derived : public Base {
+  //public:
+    //int x;
+  
+  //protected:
+    //int y;
+};
+class Base {
+  public:
+    int x;
+  
+  protected:
+    int y;
+  
+  private:
+    int z;
+};
+
+class Derived : protected Base {
+  //protected:
+    //int x;
+    //int y;
+};
+class Base {
+  public:
+    int x;
+  
+  protected:
+    int y;
+  
+  private:
+    int z;
+};
+
+class Derived : private Base {
+  //private:
+    //int x;
+    //int y;
+};
+
+// Note that private members are never inherited. 
+// Also, the examples are provided to help you visualize what the derived classes look like after inheritance. 
+// If you were to actually add the member attributes into Derived without comments, those attributes will count as being part of the Derived class instead of being inherited from the Base class.
+
+// sub exercise 
+// Accessible Functions
+
+Given the following code snippet:
+
+class Base {
+  public:
+    void SetName(string nm) {
+      name = nm;
+    }
+  
+  protected:
+    void SetAge(int a) {
+      age = a;
+    }
+    
+  private:
+    string name;
+    int age;
+};
+
+class Derived : protected Base {
+  
+};
+
+// Determine which of the following code in the main function will successfully execute.
+
+// a) 
+// Base bc;
+// bc.SetName("Peter Parker");
+
+// b) 
+// Base bc;
+// bc.SetAge(13);
+
+// c) 
+// Derived dc;
+// dc.SetName("Peter Parker");
+
+// d) 
+// Derived dc;
+// dc.SetAge(13);
+
+// answer: 
+// a) 
+
+// First, notice that Derived inherits members from Base through protected inheritance. 
+// This inheritance will cause all inherited functions in Derived to be protected. 
+// Remember, main is not part of the Base nor Derived class, which means that it can only call functions that are public. 
+// The only available public function exists within Base and that is the void SetName(string nm) function. 
+// To call this function, you’ll need to create a Base object and then call SetName on that object.
+
 
 //-----------------------------------------------------------------------------------------------------------//
 // file 2222, week 2 2/5 (Extending and Overriding)
