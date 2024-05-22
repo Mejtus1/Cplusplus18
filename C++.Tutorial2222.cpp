b17435ed17b39ec8f6033add99c8e2fde0da42ca
Mejtus1 <GitMaster1769@proton.me>
2024-05-22 12:13:44 +0200

    Base and Dervied Classes 4

--- C++.Tutorial2222.cpp
+++ C++.Tutorial2222.cpp
@@ -512,6 +512,85 @@ class Derived : protected Base {
 // The only available public function exists within Base and that is the void SetName(string nm) function. 
 // To call this function, you’ll need to create a Base object and then call SetName on that object.
 
+//-----------------------------------------------------------------------------------------------------------//
+// file 2222, week 2 1/5 (Base and Derived Classes 4) 
+
+// Inheriting the Constructor
+
+#include <iostream>
+using namespace std;
+
+class Person {
+  public:
+    Person(string n1, int a1) {
+      name = n1;
+      age = a1;
+    }
+  
+    void ReturnPerson() {
+      cout << name << endl;
+      cout << age << endl;
+    }
+  
+  private:
+    string name;
+    int age;
+};
+
+//add class definitions below this line
+
+
+
+//add class definitions above this line
+
+int main() {
+  
+  //add code below this line
+
+
+
+  //add code above this line
+  
+  return 0;
+  
+}
+
+// How are Constructors Inherited?
+// Unlike functions and attributes, the constructor that is inherited by the derived class needs to get linked or associated with that of the base class. To connect the derived constructor to the base constructor, follow this syntax:
+// Derived class name
+// Parentheses ()
+// Parameters with specified data types within parentheses
+// Colon :
+// Base class name
+// Parentheses ()
+// Parameters of derived class as arguments in parentheses
+// Any additional commands in curly braces {} or leave them empty
+// Sample Code:
+// Constructor2(Param p1, Param p2) : Constructor1(p1, p2) {}
+// Add the following code to the class definitions field:
+//add class definitions below this line
+
+class Superhero : public Person {
+  public:
+    Superhero(string n2, int a2) : Person(n2, a2) {}
+};
+
+//add class definitions above this line
+
+// And the following to main:
+
+  //add code below this line
+
+  Superhero s("Spider-Man", 16);
+  s.ReturnPerson();
+
+  //add code above this line
+
+// By associating the derived constructor with the base constructor, C++ is able to pass the parameters specified in the derived constructor as arguments of the base constructor. 
+// In the code above, the arguments Spider-Man and 16 are passed to the Superhero constructor and then transferred over to the Person constructor where they get assigned to name and age respectively. 
+// Then, the ReturnPerson function is used to print name which is now Spider-Man and age which is now 16.
+
+
 
 //-----------------------------------------------------------------------------------------------------------//
 // file 2222, week 2 2/5 (Extending and Overriding)
