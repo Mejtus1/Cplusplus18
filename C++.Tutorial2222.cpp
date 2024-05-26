37b7de74fd886c344b66f284b99d4a785aa22dfe
Mejtus1 <GitMaster1769@proton.me>
2024-05-26 13:47:02 +0200

    Base and Derived Classes exercise 2

--- C++.Tutorial2222.cpp
+++ C++.Tutorial2222.cpp
@@ -943,6 +943,39 @@ class Pet {
 
 
 //-----------------------------------------------------------------------------------------------------------//
+// file 2222, week 2 1/5 (Base and Derived Classes exercise 2) 
+
+// Deriving a Class from a Base Class
+// Given the following code:
+class Dog {
+  public:
+    Dog(string n1, string b1) {
+      name = n1;
+      breed = b1;
+    }
+
+  private:
+    string name;
+    string breed;
+};
+
+// Drag and drop the code blocks into the highlighted field below to create the derived class Pug that calls the constructor from the base class Dog with arguments.
+
+class Pug : public Dog {
+public:
+Pug(string n2, string b2) : Dog(n2, b2) {}
+};
+
+// First create the derived class, Pug, that inherits from the base class Dog. 
+// Note that public inheritance should be used over private, otherwise, the derived class constructor will not be able to access the base class constructor. 
+// The constructor of the derived class Pug should contain the same parameter types as that of the base class. 
+// Therefore, Pug needs two string parameters (i.e. string n2 and string b2). 
+// Then associate the derived class constructor with a colon : followed by the name of the base class constructor as well as its arguments and finally empty braces ({}). 
+// Note that the Pug constructor itself should be made public: and this declaration must be made before the constructor and association, otherwise, creating a Pug object within main() will not be possible. 
+// Finally, end with the ending curly brace } followed by a semicolon (;).
+
+
+//-----------------------------------------------------------------------------------------------------------//
 // file 2222, week 2 2/5 (Extending and Overriding)
 //----------------------------
 // Extending the Derived Class
