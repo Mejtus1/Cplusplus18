cc4dce3ee20c4994f56085aade1751e61013678d
Mejtus1 <GitMaster1769@proton.me>
2024-06-08 08:51:14 +0200

    exercise 3

--- C++.Tutorial2222.cpp
+++ C++.Tutorial2222.cpp
@@ -2360,3 +2360,80 @@ class BlogPost : public Book { //
     int page_views;
 };
 
+
+//-----------------------------------------------------------------------------------------------------------//
+// file 2222, week 2 5/5 (3 exercise)
+
+// Exercise 3
+// Create the class Child such that the following criteria are met:
+// Child is a derived class of Parent2 which is a derived class of Parent1
+// Override Identify in Child so that it returns This function is called from Child
+// Create the function Identify2 that invokes the Identify function from Parent2 by using the scope resolution operator (Parent2::Identify())
+// Create the function Identify3 that invokes the Identify function from Parent1 by using the scope resolution operator (Parent1::Identify())
+
+#include <iostream>
+using namespace std;
+
+//DO NOT EDIT code below this line
+
+class Parent1 {
+  public:
+    string Identify() {
+      return "This function is called from Parent1";
+    }
+};
+
+class Parent2 : public Parent1 {
+  public:
+    string Identify() {
+      return "This function is called from Parent2";
+    }
+};
+
+//DO NOT EDIT code above this line
+
+//add class definitions below this line
+
+
+
+//add class definitions above this line
+
+int main() {
+  
+  //DO NOT EDIT code below this line
+
+  Child c;
+  cout << c.Identify() << endl;
+  cout << c.Identify2() << endl;
+  cout << c.Identify3() << endl;
+  
+  //DO NOT EDIT code above this line
+  
+  return 0;
+  
+}
+
+// Expected Output
+// This function is called from Child
+// This function is called from Parent2
+// This function is called from Parent1
+
+class Child : public Parent2 {
+  public:
+    string Identify() {
+      return "This function is called from Child";
+    }
+  
+    string Identify2() {
+      return Parent2::Identify();
+    }
+  
+    string Identify3() {
+      return Parent1::Identify();
+    }
+};
+
+// Create the class Child that inherits from Parent2
+// All of the Child class functions are string because they each return one
+// Use the scope resolution operator Parent2::Identify() to access Identify() in the Parent2 class
+// Use the scope resolution operator Parent1::Identify() to access Identify() in the Parent1 class
