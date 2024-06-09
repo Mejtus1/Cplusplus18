f1810c389e43c663c08a2351749572cb7f6654ce
Mejtus1 <GitMaster1769@proton.me>
2024-06-09 23:42:35 +0200

    exercise 4

--- C++.Tutorial2222.cpp
+++ C++.Tutorial2222.cpp
@@ -2437,3 +2437,132 @@ class Child : public Parent2 {
 // All of the Child class functions are string because they each return one
 // Use the scope resolution operator Parent2::Identify() to access Identify() in the Parent2 class
 // Use the scope resolution operator Parent1::Identify() to access Identify() in the Parent1 class
+
+//-----------------------------------------------------------------------------------------------------------//
+// file 2222, week 2 5/5 (4 exercise)
+
+// Exercise 4
+// Use the base class PiggyBank to help you solve this exercise. Create the derived class Cash so that it has the following functions:
+// DisplayBills - function that prints all of the dollar bills’ quantity in order of increasing amount
+// DisplayCashTotal - function that prints the value of all of the dollar bills combined
+
+#include <iostream>
+using namespace std;
+
+//DO NOT EDIT code below this line
+
+class PiggyBank {
+  public:
+    PiggyBank(int o, int f, int tn, int tw) {
+      ones = o;
+      fives = f;
+      tens = tn;
+      twenties = tw;
+    }
+  
+    int GetOnes() {
+      return ones;
+    }
+  
+    void SetOnes(int new_ones) {
+      ones = new_ones;
+    }
+  
+    int GetFives() {
+      return fives;
+    }
+  
+    void SetFives(int new_fives) {
+      fives = new_fives;
+    }
+  
+    int GetTens() {
+      return tens;
+    }
+  
+    void SetTens(int new_tens) {
+      tens = new_tens;
+    }
+  
+    int GetTwenties() {
+      return twenties;
+    }
+  
+    void SetTwenties(int new_twenties) {
+      twenties = new_twenties;
+    }
+  
+  private:
+    int ones;
+    int fives;
+    int tens;
+    int twenties;
+};
+
+//DO NOT EDIT code above this line
+
+//add class definitions below this line
+
+//DO NOT EDIT/////////////////////
+class Cash : public PiggyBank { //
+//////////////////////////////////
+
+  
+  
+  
+  
+//add class definitions above this line
+
+int main() {
+  
+  //DO NOT EDIT code below this line
+
+  Cash c(1, 2, 3, 4);
+  c.DisplayBills();
+  c.DisplayCashTotal();
+  Cash c2(9, 8, 7, 6);
+  c2.DisplayBills();
+  c2.DisplayCashTotal();
+
+  //DO NOT EDIT code above this line
+  
+  return 0;
+  
+}
+
+// Expected Output
+// One-dollar bill: 1
+// Five-dollar bill: 2
+// Ten-dollar bill: 3
+// Twenty-dollar bill: 4
+// Total amount: 121
+// One-dollar bill: 9
+// Five-dollar bill: 8
+// Ten-dollar bill: 7
+// Twenty-dollar bill: 6
+// Total amount: 239
+
+//DO NOT EDIT/////////////////////
+class Cash : public PiggyBank { //
+//////////////////////////////////
+  public:
+    Cash(int o, int f, int tn, int tw) : PiggyBank(o, f, tn, tw) {}
+  
+    void DisplayBills() {
+      cout << "One-dollar bill: " << GetOnes() << endl;
+      cout << "Five-dollar bill: " << GetFives() << endl;
+      cout << "Ten-dollar bill: " << GetTens() << endl;
+      cout << "Twenty-dollar bill: " << GetTwenties() << endl;
+    }
+  
+    void DisplayCashTotal() {
+      cout << "Total amount: "; 
+      cout << GetOnes() + GetFives() * 5 + GetTens() * 10 + GetTwenties() * 20;
+      cout << endl;
+    }
+};
+
+// Connect the derived and base constructors using Cash(int o, int f, int tn, int tw) : PiggyBank(o, f, tn, tw) {}
+// DisplayBills and DisplayCashTotal should be void functions since they do not return a data type
+// DisplayBills uses getters from the PiggyBank class to print the bills’ quantity
+// DisplayCashTotal multiplies each dollar bill quantity by its value, then adds the individual totals to print the grand total amount
